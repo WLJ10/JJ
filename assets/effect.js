85cd19abd57f58c3de327e525349ec32a9f05ba3
Pesion <2357394024@qq.com>
2024-09-29 08:02:11 +0000

    init

---
+++ assets/effect.js
@@ -0,0 +1,230 @@
+$(window).load(function(){
+	$('.loading').fadeOut('fast');
+	$('.container').fadeIn('fast');
+});
+$('document').ready(function(){
+		var vw;
+		$(window).resize(function(){
+			 vw = $(window).width()/2;
+			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+			$('#b11').animate({top:240, left: vw-350},500);
+			$('#b22').animate({top:240, left: vw-250},500);
+			$('#b33').animate({top:240, left: vw-150},500);
+			$('#b44').animate({top:240, left: vw-50},500);
+			$('#b55').animate({top:240, left: vw+50},500);
+			$('#b66').animate({top:240, left: vw+150},500);
+			$('#b77').animate({top:240, left: vw+250},500);
+		});
+
+	$('#turn_on').click(function(){
+		$('#bulb_yellow').addClass('bulb-glow-yellow');
+		$('#bulb_red').addClass('bulb-glow-red');
+		$('#bulb_blue').addClass('bulb-glow-blue');
+		$('#bulb_green').addClass('bulb-glow-green');
+		$('#bulb_pink').addClass('bulb-glow-pink');
+		$('#bulb_orange').addClass('bulb-glow-orange');
+		$('body').addClass('peach');
+		$(this).fadeOut('slow').delay(300).promise().done(function(){
+			$('#play').fadeIn('slow');
+		});
+	});
+	//播放音乐
+	$('#play').click(function(){
+		var audio = $('.song')[0];
+        audio.play();
+        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
+		$('#bulb_red').addClass('bulb-glow-red-after');
+		$('#bulb_blue').addClass('bulb-glow-blue-after');
+		$('#bulb_green').addClass('bulb-glow-green-after');
+		$('#bulb_pink').addClass('bulb-glow-pink-after');
+		$('#bulb_orange').addClass('bulb-glow-orange-after');
+		$('body').css('backgroud-color','#FFF');
+		$('body').addClass('peach-after');
+		$(this).fadeOut('slow').delay(300).promise().done(function(){
+			$('#bannar_coming').fadeIn('slow');
+		});
+	});
+	//皇冠
+	$('#bannar_coming').click(function(){
+		$('.bannar').addClass('bannar-come');
+		$(this).fadeOut('slow').delay(300).promise().done(function(){
+			$('#balloons_flying').fadeIn('slow');
+		});
+	});
+
+	function loopOne() {
+		var randleft = window.innerWidth*Math.random();
+		var randtop = window.innerHeight*Math.random();
+		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
+			loopOne();
+		});
+	}
+	function loopTwo() {
+		var randleft = window.innerWidth*Math.random();
+		var randtop = window.innerHeight*Math.random();
+		$('#b2').animate({left:randleft,bottom:randtop},10000,function(){
+			loopTwo();
+		});
+	}
+	function loopThree() {
+		var randleft = window.innerWidth*Math.random();
+		var randtop = window.innerHeight*Math.random();
+		$('#b3').animate({left:randleft,bottom:randtop},10000,function(){
+			loopThree();
+		});
+	}
+	function loopFour() {
+		var randleft = window.innerWidth*Math.random();
+		var randtop = window.innerHeight*Math.random();
+		$('#b4').animate({left:randleft,bottom:randtop},10000,function(){
+			loopFour();
+		});
+	}
+	function loopFive() {
+		var randleft = window.innerWidth*Math.random();
+		var randtop = window.innerHeight*Math.random();
+		$('#b5').animate({left:randleft,bottom:randtop},10000,function(){
+			loopFive();
+		});
+	}
+
+	function loopSix() {
+		var randleft = window.innerWidth*Math.random();
+		var randtop = window.innerHeight*Math.random();
+		$('#b6').animate({left:randleft,bottom:randtop},10000,function(){
+			loopSix();
+		});
+	}
+	function loopSeven() {
+		var randleft = window.innerWidth*Math.random();
+		var randtop = window.innerHeight*Math.random();
+		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
+			loopSeven();
+		});
+	}
+	// 随机气球
+	$('#balloons_flying').click(function(){
+		$('.balloon-border').animate({top:-500},8000);//上升气球群
+		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
+		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
+		// $('#b3').addClass('balloons-rotate-behaviour-two');
+		// $('#b4').addClass('balloons-rotate-behaviour-one');
+		// $('#b5').addClass('balloons-rotate-behaviour-one');
+		// $('#b6').addClass('balloons-rotate-behaviour-two');
+		// $('#b7').addClass('balloons-rotate-behaviour-one');
+		loopOne();
+		loopTwo();
+		loopThree();
+		loopFour();
+		loopFive();
+		loopSix();
+		loopSeven();
+		
+		$(this).fadeOut('slow').delay(500).promise().done(function(){
+			$('#cake_fadein').fadeIn('slow');
+		});
+	});	
+
+	$('#cake_fadein').click(function(){
+		$('.cake').fadeIn('slow');
+		$(this).fadeOut('slow').delay(300).promise().done(function(){
+			$('#light_candle').fadeIn('slow');
+		});
+	});
+
+	$('#light_candle').click(function(){
+		$('.fuego').fadeIn('slow');
+		$(this).fadeOut('slow').promise().done(function(){
+			$('#wish_message').fadeIn('slow');
+		});
+	});
+
+		
+	$('#wish_message').click(function(){
+		 vw = $(window).width()/2;
+
+		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+		$('#b1').attr('id','b11');
+		$('#b2').attr('id','b22')
+		$('#b3').attr('id','b33')
+		$('#b4').attr('id','b44')
+		$('#b5').attr('id','b55')
+		$('#b6').attr('id','b66')
+		$('#b7').attr('id','b77')
+		$('#b11').animate({top:240, left: vw-350},500);
+		$('#b22').animate({top:240, left: vw-250},500);
+		$('#b33').animate({top:240, left: vw-150},500);
+		$('#b44').animate({top:240, left: vw-50},500);
+		$('#b55').animate({top:240, left: vw+50},500);
+		$('#b66').animate({top:240, left: vw+150},500);
+		$('#b77').animate({top:240, left: vw+250},500);
+		$('.balloons').css('opacity','0.9');
+		$('.balloons').css('position','absolute');
+		$('.balloons h2').fadeIn(300);
+		$(this).fadeOut('slow').delay(300).promise().done(function(){
+			$('#story').fadeIn('slow');
+		});
+	});
+	
+	$('#story').click(function(){
+		$(this).fadeOut('slow');
+		$('.cake').fadeOut('fast').promise().done(function(){
+			$('.message').fadeIn('slow');
+			$('.article').fadeIn('slow');
+
+		});
+		
+		var i;
+
+		function msgLoop (parentSelector, i, callback) {
+			$(`${parentSelector} p:nth-of-type(${i})`).fadeOut('slow').delay(80).promise().done(function() {
+			i = i + 1;
+			// 再次使用反引号插入新的 i
+			$(`${parentSelector} p:nth-of-type(${i})`).fadeIn('slow').delay(100);
+			if(i==50){
+				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+					$('.cake').fadeIn('fast');
+					// 当整个 msgLoop 执行完时调用回调
+					if (typeof callback === 'function') {
+						callback(); // 调用回调函数
+					}
+				});
+				
+			}
+			else{
+				msgLoop(parentSelector, i, callback);
+			}			
+
+		});
+	}
+		function articleShow (parentSelector, i) {
+			// 再次使用反引号插入新的 i
+			$(`${parentSelector} p:nth-of-type(${i})`).fadeIn('slow').delay(100).promise().done(function(){
+			i = i + 1;
+			if(i==50){
+				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+					// 当整个 msgLoop 执行完时调用回调
+				});
+				
+			}
+			else{
+				articleShow(parentSelector, i);
+			}		
+		});	
+
+			// body...
+		};
+		msgLoop("#texts-container",0, function(){
+		articleShow('#article-container',0);
+		});
+		// $('.message').fadeIn('slow')
+
+		// $('.article').fadeIn('slow');
+		
+	});
+});
+
+
+
+
+//alert('hello');
